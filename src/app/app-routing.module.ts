825a1de1ed4018d54fc4b395d0b89ea803b1cef0
AmonMcDuul <joranomic@hotmail.com>
2023-06-09 20:51:30 +0200

    store wip

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LobbyComponent } from './components/lobby/lobby.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    component: LobbyComponent,
+    data: { state: "lobby" },
+  },
+];
+  @NgModule({
+    imports: [
+      RouterModule.forRoot(routes, {
+        scrollPositionRestoration: "enabled",
+        onSameUrlNavigation: "reload",
+        enableTracing: false,
+      }),
+    ],
+    exports: [RouterModule],
+  })
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
 export class AppRoutingModule { }
---
+++ src/app/store/services/signal-r.models.ts
@@ -0,0 +1,8 @@
+export interface CreateGroepModel {
+    groupName: string;
+  }
+
+export interface GroupCreatedModel {
+    groupId: string;
+    groupName: string;
+}
