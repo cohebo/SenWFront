c501e40d559f68fb3dc30aef58d7e6fece1bcbd4
Subtracting <subtracting@gmail.com>
2023-07-24 21:42:24 +0200

    getLocation frontend function

--- src/app/components/new-player/new-player.component.ts
+++ src/app/components/new-player/new-player.component.ts
@@ -16,6 +16,10 @@ import { CreatePlayerModel } from 'src/app/store/services/signal-r.models';
 export class NewPlayerComponent implements OnInit {
   sliderValue: number = 0;
 
+  textContent: string = ''
+  latitude: number | undefined;
+  longitude: number | undefined;
+
   onSliderChange(event: any) {
     this.sliderValue = Number(event.target.value);
   }
@@ -35,12 +39,44 @@ export class NewPlayerComponent implements OnInit {
     this.store.dispatch(startConnection());
   }
 
+  // TO DO: GetLocation naar service
+  getLocation() {
+ 
+    const success = (position: any) => {
+      this.latitude = position.coords.latitude;
+      this.longitude = position.coords.longitude;
+      console.log(this.latitude, this.longitude);
+    };
+  
+    const error = () => {
+      this.textContent = "Unable to retrieve your location";
+      this.displayError(this.textContent);
+    };
+  
+    if (!navigator.geolocation) {
+      this.textContent = "Geolocation is not supported by your browser";
+      this.displayError(this.textContent);
+    } else {
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  }
+  
+  displayError(message: string) {
+    console.log(message)
+  }
+
+
   createNewPlayer() {
     if (this.newPlayerForm.valid) {
+
+      this.getLocation();
+
       const player: CreatePlayerModel = {
         playerName: this.newPlayerForm.value.newPlayerName,
       };
       this.store.dispatch(createPlayer(player));
     }
+
+
   }
 }
